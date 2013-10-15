e2ed340f41a7a1cc8d6c0cf9bbfa68510975f937
Rustam Azimov <rustam.azimov19021995@gmail.com>
2013-10-15 23:03:58 +0400

    Make gui project and added PlotWidget for work with data.

---
+++ gui/plotWidget.cpp
@@ -0,0 +1,102 @@
+#include "plotWidget.h"
+
+#include <qwt_plot_curve.h>
+#include <qwt_plot_grid.h>
+#include <qwt_plot_canvas.h>
+#include <qwt_scale_div.h>
+
+#include <QDebug>
+
+const int queueSize = 400;
+
+PlotWidget::PlotWidget() :
+	mOriginalCurve(new QwtPlotCurve),
+	mFilteredCurve(new QwtPlotCurve)
+{
+	for (int i = 0; i < queueSize; i++) {
+		mIterator.append(i);
+	}
+
+	this->enableAxis(QwtPlot::xBottom, false);
+	this->setCanvasBackground(Qt::white);
+
+
+	setAxisMaxMajor(QwtPlot::yLeft, queueSize / 8);
+	setAxisMaxMajor(QwtPlot::xBottom, queueSize / 8);
+
+	mGrid = new QwtPlotGrid;
+	mGrid->setPen("#999999", 0.2);
+	mGrid->attach(this);
+
+	mOriginalCurve->attach(this);
+	mOriginalCurve->setPen(Qt::blue, 2);
+	mOriginalCurve->setRenderHint(QwtPlotItem::RenderAntialiased, true);
+
+	mFilteredCurve->attach(this);
+	mFilteredCurve->setVisible(false);
+	mFilteredCurve->setPen("#33cc33", 2);
+	mFilteredCurve->setRenderHint(QwtPlotItem::RenderAntialiased, true);;
+
+}
+
+PlotWidget::~PlotWidget()
+{
+	delete mOriginalCurve;
+	delete mFilteredCurve;
+	delete mGrid;
+}
+
+void PlotWidget::addOriginalPoint(const int &newPoint)
+{
+	if (mOriginalData.size() >= queueSize) {
+		mOriginalData.dequeue();
+	}
+
+	mOriginalData.enqueue(newPoint);
+
+	mOriginalCurve->setSamples(mIterator.toVector(), mOriginalData.toVector());
+	this->replot();
+}
+
+void PlotWidget::addFilteredPoint(const int &newPoint)
+{
+	if (mFilteredData.size() >= queueSize) {
+		mFilteredData.dequeue();
+	}
+
+	mFilteredData.enqueue(newPoint);
+
+	if (mFilteredCurve->isVisible()) {
+		mFilteredCurve->setSamples(mIterator.toVector(), mFilteredData.toVector());
+		this->replot();
+	}
+}
+
+void PlotWidget::setFilteredVisible(bool isVisible)
+{
+	mFilteredCurve->setVisible(isVisible);
+}
+
+void PlotWidget::updateOriginalPoint()
+{
+	if (mOriginalData.isEmpty())
+	{
+		addOriginalPoint(qrand() % 11 - 4);
+	}
+	else {
+		addOriginalPoint(mOriginalData. last() + qrand() % 11 - 4);
+	}
+}
+
+void PlotWidget::updateFilteredPoint()
+{
+	if (mFilteredData.isEmpty())
+	{
+		addFilteredPoint(qrand() % 11 - 4);
+	}
+	else {
+		addFilteredPoint(mFilteredData. last() + qrand() % 11 - 4);
+	}
+}
+
+
---
+++ gui/plotWidget.h
@@ -0,0 +1,40 @@
+#ifndef PLOTWIDGET_H
+#define PLOTWIDGET_H
+
+#include <QList>
+#include <QQueue>
+#include <QPointF>
+#include "qwt_plot.h"
+
+class QwtPlotCurve;
+class QwtPlotGrid;
+
+class PlotWidget : public QwtPlot
+{
+	Q_OBJECT
+public:
+	PlotWidget();
+	~PlotWidget();
+
+	void addOriginalPoint(const int &newPoint);
+	void addFilteredPoint(const int &newPoint);
+
+	void setFilteredVisible(bool isVisible);
+
+public slots:
+	void updateOriginalPoint();
+	void updateFilteredPoint();
+
+private:
+	QwtPlotCurve *mOriginalCurve;
+	QwtPlotCurve *mFilteredCurve;
+
+	QwtPlotGrid* mGrid;
+
+	QQueue<qreal> mOriginalData;
+	QQueue<qreal> mFilteredData;
+
+	QList<qreal> mIterator;
+};
+
+#endif // PLOTWIDGET_H
